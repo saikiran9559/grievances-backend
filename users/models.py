6aebdc652202944957936efbfa3aa36c8815b5c8
MurariSabavath <smurari1004@gmail.com>
2022-06-17 18:08:06 +0530

    user authentication

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from statistics import mode
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __init__(self):
+        return self.user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
+from django.contrib.auth.models import User
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+@api_view(['GET'])
+def home(request):
+    return Response({'data': 'hello world'})
+
+
+class UserList(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()

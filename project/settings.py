d11048a4352d623308b3ccf26983a7c95d875338
MurariSabavath <smurari1004@gmail.com>
2022-06-17 17:51:25 +0530

    add user app

--- project/settings.py
+++ project/settings.py
@@ -25,8 +25,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'users',
+
+    # third party
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [

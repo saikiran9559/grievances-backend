ec9c8301b424ee872a9da8b3f52fe6966c96ac5f
MurariSabavath <smurari1004@gmail.com>
2022-06-20 20:01:23 +0530

    added protected views

--- project/settings.py
+++ project/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
